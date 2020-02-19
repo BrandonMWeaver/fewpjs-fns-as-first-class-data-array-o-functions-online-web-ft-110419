1563261a0e42fcfe8eb7fbe5a0aaa226c8acd100
Brandon Weaver <a7xncob@gmail.com>
2020-02-19 17:40:55 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function wakeDog()
+{
+  console.log("Wake Byron the poodle");
+}
+
+function leashDog()
+{
+  console.log("Leash Byron the poodle");
+}
+
+function walkToPark()
+{
+  console.log();
+}
+
+function throwFrisbee()
+{
+  console.log();
+}
+
+function walkHome()
+{
+  console.log();
+}
+
+function unleashDog()
+{
+  console.log();
+}
