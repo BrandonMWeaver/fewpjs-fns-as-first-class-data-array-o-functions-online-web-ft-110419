c2dea4cb6c87869d9d7de20d45a282bf66a5a426
Brandon Weaver <a7xncob@gmail.com>
2020-02-19 17:45:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-function wakeDog()
+function wakeDog(dogName, dogBreed)
 {
-  console.log("Wake Byron the poodle");
+  console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 
-function leashDog()
+function leashDog(dogName, dogBreed)
 {
-  console.log("Leash Byron the poodle");
+  console.log(`Leash ${dogName} the ${dogBreed}`);
 }
 
-function walkToPark()
+function walkToPark(dogName, dogBreed)
 {
-  console.log("Walk to the park with Byron the poodle");
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
 }
 
-function throwFrisbee()
+function throwFrisbee(dogName, dogBreed)
 {
-  console.log("Throw the frisbee for Byron the poodle");
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
 }
 
-function walkHome()
+function walkHome(dogName, dogBreed)
 {
-  console.log("Walk home with Byron the poodle");
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
 }
 
-function unleashDog()
+function unleashDog(dogName, dogBreed)
 {
-  console.log("Unleash Byron the poodle");
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
 }
